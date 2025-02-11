bb5cb3200de27ff1b4cf0cf41f237c09ac3a509b
Diego <58009401+marzo245@users.noreply.github.com>
2025-02-11 11:12:44 -0500

    Documentando que hace cada parte del codigo

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -11,24 +11,32 @@ import java.util.Queue;
  *
  * @author hcadavid
  */
-public class Consumer extends Thread{
-    
-    private Queue<Integer> queue;
-    
-    
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
-    }
-    
-    @Override
-    public void run() {
-        while (true) {
+public class Consumer extends Thread {
 
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
-            }
-            
-        }
+  // Cola de enteros
+  private Queue<Integer> queue;
+
+  /*
+   * Constructor de la clase Consumer
+   * @param queue Cola de enteros
+   */
+  public Consumer(Queue<Integer> queue) {
+    this.queue = queue;
+  }
+
+  /*
+   * Metodo que se encarga de consumir los elementos de la cola
+   * Este metodo se ejecuta cuando el hilo es iniciado en un bucle infinito,
+   * verifica si hay elementos en la cola. Si hay, consume el primer elemento(usando poll, que tambien
+   * lo elimina de la cola) e imprime un mensaje.
+   */
+  @Override
+  public void run() {
+    while (true) {
+      if (queue.size() > 0) {
+        int elem = queue.poll();
+        System.out.println("Consumer consumes " + elem);
+      }
     }
+  }
 }
--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -16,32 +16,44 @@ import java.util.logging.Logger;
  */
 public class Producer extends Thread {
 
-    private Queue<Integer> queue = null;
+  // Cola de enteros
+  private Queue<Integer> queue = null;
+  // Semilla de datos que va a incrementar
+  private int dataSeed = 0;
+  // Random para generar numeros aleatorios
+  private Random rand = null;
+  // Limite de stock (No se usa)
+  private final long stockLimit;
 
-    private int dataSeed = 0;
-    private Random rand=null;
-    private final long stockLimit;
+  /*
+   * Constructor de la clase Producer
+   * @param queue Cola de enteros
+   * @param stockLimit Limite de stock
+   */
+  public Producer(Queue<Integer> queue, long stockLimit) {
+    this.queue = queue;
+    // Inicia el random con la semilla del tiempo actual
+    rand = new Random(System.currentTimeMillis());
+    this.stockLimit = stockLimit;
+  }
 
-    public Producer(Queue<Integer> queue,long stockLimit) {
-        this.queue = queue;
-        rand = new Random(System.currentTimeMillis());
-        this.stockLimit=stockLimit;
+  /*
+   * En un bucle infinito, genera un nuevo número aleatorio,
+   * lo añade a la cola y duerme el hilo durante 1 segundo.
+   * Imprime el número que ha añadido.
+   */
+  @Override
+  public void run() {
+    while (true) {
+      dataSeed = dataSeed + rand.nextInt(100);
+      System.out.println("Producer added " + dataSeed);
+      queue.add(dataSeed);
+
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException ex) {
+        Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
-
-    @Override
-    public void run() {
-        while (true) {
-
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        }
-    }
+  }
 }
